1a60a180ee2db1b376eb532013f0bd2a6a95d43b
Simon Alzate Velasquez <simon_alzatev@soy.sena.edu.co>
2025-02-06 00:39:45 -0500

    FIrst Concepts

---
+++ POOconcepts/POOconcepts/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using POOconcepts;
+
+Console.WriteLine("Hello, World!");
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(2014, 2, 29));
+	Console.WriteLine(new Date(1999, 21, 30));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
+
+
+
